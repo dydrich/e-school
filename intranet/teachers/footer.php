96c0aa7a7ae259b6a496a2b2d0d03c635b3360d9
dydrich <dydrich@yahoo.com>
2014-09-04 17:24:30 +0200

    aggiunto il codice html per la nuova gestione delle notifiche

--- intranet/teachers/footer.php
+++ intranet/teachers/footer.php
@@ -1,3 +1,15 @@
 <footer id="footer">
 <span>Copyright <?php echo date("Y") ?> Riccardo Bachis | <a href="<?php print $_SESSION['__config__']['root_site'] ?>"><?php print $_SESSION['__config__']['intestazione_scuola'] ?></a></span>
 </footer>
+<div id="alert" style="display: none">
+	<p>
+		<span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
+		<span class="_bold" id="alertmessage" style="font-size: 1.2em"></span>
+	</p>
+</div>
+<div id="error" style="display: none">
+	<p>
+		<span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>
+		<span class="_bold" id="errormessage" style="font-size: 1.2em"></span>
+	</p>
+</div>
