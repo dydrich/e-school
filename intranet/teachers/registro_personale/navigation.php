003acaa90e3bd25b0103b29ec6366d8832876aa3
dydrich <dydrich@yahoo.com>
2014-12-15 12:00:07 +0100

    correzioni grafiche

--- intranet/teachers/registro_personale/navigation.php
+++ intranet/teachers/registro_personale/navigation.php
@@ -36,7 +36,7 @@ $is_teacher_in_this_class = $_SESSION['__user__']->isTeacherInClass($_SESSION['_
 <nav id="navigation">
 	<div id="head_label">
 		<img src="<?php echo $_SESSION['__path_to_root__'] ?>images/ic_navigation_drawer3.png" id="open_drawer" style="float: left; position: relative; top: 18px" />
-		<p id="drawer_label" style="margin-top: 17px; vertical-align: top; font-weight: bold; ; margin-left: 10px; float: left; color: white"><?php echo $drawer_label ?></p>
+		<p id="drawer_label" style="margin-top: 17px; vertical-align: top; margin-left: 10px; float: left; color: white"><?php echo $drawer_label ?></p>
 	</div>
 	<div class="nav_div" style="float: right; margin-right: 50px; position: relative; top: 20px; text-align: right">Area docenti::<span id="navlabel"><?php echo $navigation_label ?></span></div>
 	<div class="nav_div" style="clear: both"></div>
