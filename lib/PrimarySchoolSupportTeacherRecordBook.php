e13f158cdd64528140f94eb08d5ff5c324c738fd
dydrich <dydrich@yahoo.com>
2015-06-10 17:38:12 +0200

    bug fixes creazione registro di sostegno

--- lib/PrimarySchoolSupportTeacherRecordBook.php
+++ lib/PrimarySchoolSupportTeacherRecordBook.php
@@ -65,12 +65,11 @@ class PrimarySchoolSupportTeacherRecordBook extends PrimarySchoolTeacherRecordBo
 		$student['indirizzo'] = "";
 		$student['citta'] = "";
 		$student['telefono'] = "";
-		$_address = $this->datasource->executeQuery("SELECT indirizzo, citta, telefono1 FROM rb_indirizzi_alunni WHERE id_alunno = {$st}");
+		$_address = $this->datasource->executeQuery("SELECT indirizzo, citta FROM rb_indirizzi_alunni WHERE id_alunno = {$st}");
 		if ($_address){
 			$address = $_address[0];
 			$student['indirizzo'] = $address['indirizzo'];
 			$student['citta'] = $address['citta'];
-			$student['telefono'] = $address['telefono1'];
 		}
 
 		$student['sostegno'] = array();
