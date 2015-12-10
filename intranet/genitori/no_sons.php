6d957cb422843d827616a6a8db05a668d21610b5
dydrich <dydrich@yahoo.com>
2015-12-10 11:54:40 +0100

    controllo accesso genitori senza figli associati

--- intranet/genitori/no_sons.php
+++ intranet/genitori/no_sons.php
@@ -31,15 +31,20 @@ $drawer_label = "Problema di accesso";
 <div id="main">
 	<div id="right_col">
 		<div class="smallbox" id="working">
-			<p class="menu_label class_icon">PProblema di accesso</p>
+			<p class="menu_label class_icon">Problema di accesso</p>
+			<ul class="menublock" style="" dir="rtl">
+				<li><a href="mailto:<?php echo $_SESSION['__config__']['admin_email'] ?>?subject=Genitore non associato">Segnala il problema</a></li>
+				<li><a href="../../shared/do_logout.php">Logout</a></li>
+				<li><a href="<?php print $_SESSION['__config__']['root_site'] ?>">Home page</a></li>
+			</ul>
 		</div>
 	</div>
 	<div id="left_col">
 		<div id="welcome">
 			<p id="w_head" class="attention" style="font-weight: bold; font-size: 1.3em">Attenzione</p>
 			<p class="w_text attention" style="font-weight: bold">Hai eseguito l'accesso con un account Genitore, ma non risulta alcun alunno a te associato.</p>
-			<p class="w_text attention">Se credi si tratti di un errore, <a href='mailto:<?php echo $_SESSION['__config__']['admin_email'] ?>?subject=Genitore non associato' style='text-decoration: underline'>contatta l'amministratore</a></p>
-			<p class="w_text">&middot; Oppure <a href="../../shared/do_logout.php">torna alla pagina di login.</a></p>
+			<p class="w_text attention">Se credi si tratti di un errore, <a href='mailto:<?php echo $_SESSION['__config__']['admin_email'] ?>?subject=Genitore non associato'>contatta l'amministratore</a></p>
+			<p class="w_text"><a href="../../shared/do_logout.php">Torna alla pagina di login</a> oppure, se hai altri profili di accesso, <a href="<?php print $_SESSION['__config__']['root_site'] ?>">alla home</a>.</p>
 		</div>
 	</div>
 	<p class="spacer"></p>
