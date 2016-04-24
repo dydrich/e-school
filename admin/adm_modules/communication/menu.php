bd96c1fa1d5d8581a046954ba1ca0d4342e3ff18
dydrich <dydrich@yahoo.com>
2016-04-24 10:40:30 +0200

    spostamento amministrazione modulo communication

--- admin/adm_modules/communication/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="smallbox" id="working">
-	<p class="menu_label com_icon">Comunicazioni</p>
-	<ul class="menublock" style="" dir="rtl">
-		<li><a href="<?php echo $_SESSION['__path_to_mod_home__'] ?>index.php?tab=10">Home</a></li>
-		<li><a href="groups.php" id="tab_1">Gruppi</a></li>
-	</ul>
-</div>
