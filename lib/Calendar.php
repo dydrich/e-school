488333d0f6b455d462b35f4be4e406d34e93bdaa
Riccardo Bachis <dydrich@yahoo.com>
2016-09-25 17:07:20 +0200

    aggiunte le attvita (tipo impegno = 2)

--- lib/Calendar.php
+++ lib/Calendar.php
@@ -26,17 +26,18 @@ class Calendar
         $this->subject = $subj;
         $this->datasource = $db;
 
-        $this->loadActivities();
+        $this->loadData();
     }
 
-    private function loadActivities() {
+    private function loadData() {
         if ($this->class != null || $this->teacher != null) {
             $this->loadHomeworks();
+			$this->loadActivities();
         }
     }
 
     private function loadHomeworks() {
-        $sel_act = "SELECT rb_impegni.*, rb_materie.materia AS mat 
+        $sel_act = "SELECT rb_impegni.*, DATE(rb_impegni.data_inizio) AS start_date, rb_materie.materia AS mat 
                     FROM rb_impegni, rb_materie 
                     WHERE rb_materie.id_materia = rb_impegni.materia
                     AND data_inizio >= NOW() 
@@ -50,16 +51,44 @@ class Calendar
         if ($this->subject != null) {
             $sel_act .= " AND rb_impegni.materia = ".$this->subject;
         }
-        $sel_act .= " ORDER BY data_inizio";
+        $sel_act .= " ORDER BY data_inizio DESC";
 
         $hws = $this->datasource->executeQuery($sel_act);
         foreach ($hws as $hw) {
-            if (!isset($this->activities[$hw['data_inizio']])) {
-                $this->activities[$hw['data_inizio']] = [];
+            if (!isset($this->activities[$hw['start_date']])) {
+                $this->activities[$hw['start_date']] = [];
             }
-            $this->activities[$hw['data_inizio']][] = $hw;
+            $this->activities[$hw['start_date']][] = $hw;
         }
     }
+
+    private function loadActivities() {
+		$sel_act = "SELECT rb_impegni.*, DATE(rb_impegni.data_inizio) AS start_date,
+					rb_materie.materia AS mat 
+					FROM rb_impegni LEFT JOIN rb_materie 
+					ON rb_materie.id_materia = rb_impegni.materia 
+					WHERE classe = ".$this->class." 
+					AND data_fine >= NOW() 
+					AND rb_impegni.tipo = 1 ";
+		if ($this->class != null) {
+			$sel_act .= " AND classe = ".$this->class;
+		}
+		if ($this->teacher != null) {
+			$sel_act .= " AND docente = ".$this->teacher;
+		}
+		if ($this->subject != null) {
+			$sel_act .= " AND rb_impegni.materia = ".$this->subject;
+		}
+		$sel_act .= " ORDER BY data_inizio DESC";
+
+		$acts = $this->datasource->executeQuery($sel_act);
+		foreach ($acts as $hw) {
+			if (!isset($this->activities[$hw['start_date']])) {
+				$this->activities[$hw['start_date']] = [];
+			}
+			$this->activities[$hw['start_date']][] = $hw;
+		}
+	}
 
     public function getActivities() {
         return $this->activities;
