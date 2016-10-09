cf4c5fdbdc6e9069347b1e88a6c3726ce93c246b
Riccardo Bachis <dydrich@yahoo.com>
2016-10-09 22:58:27 +0200

    milestone ATA (issue#151, issue#160)

--- intranet/ata/menu.php
+++ intranet/ata/menu.php
@@ -1,6 +1,6 @@
 <div class="smallbox" id="working">
 	<p class="menu_label class_icon">La tua scuola</p>
 	<ul class="menublock" style="" dir="rtl">
-		<li><a href="avvisi_dsga.php">Avvisi DSGA</a></li>
+		<li><a href="<?php echo $_SESSION['__path_to_root__'] ?>modules/communication/load_module.php?module=com&area=<?php echo $_SESSION['__area__'] ?>&page=archived_notices">Avvisi DSGA</a></li>
 	</ul>
 </div>
