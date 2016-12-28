d2d72df3ba70e5d37f2d0469973e23918af77a04
Riccardo Bachis <dydrich@yahoo.com>
2016-12-28 13:09:19 +0100

    issue #188: modifica gestione avvisi

---
+++ shared/get_classes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: riccardo
+ * Date: 22/02/14
+ * Time: 19.23
+ * get all classes
+ */
+
+require_once "../lib/start.php";
+
+check_session();
+
+$params = '';
+if (isset($_POST['school_level'])) {
+	$params = ' AND rb_classi.ordine_di_scuola = '.$_POST['school_level']." ";
+}
+
+$sel_classi = "SELECT id_classe, anno_corso, sezione, codice, nome 
+			  FROM rb_classi, rb_tipologia_scuola, rb_sedi 
+			  WHERE id_sede = sede 
+			  AND rb_classi.ordine_di_scuola = id_tipo $params
+			  ORDER BY sezione, anno_corso";
+$res_classi = $db->executeQuery($sel_classi);
+$classi = array();
+while ($row = $res_classi->fetch_assoc()){
+	$classi[] = array("id" => $row['id_classe'], "classe" => $row['anno_corso'].$row['sezione'], "sede" => $row['nome']);
+}
+
+$response = array("status" => "ok", "message" => "", "data" => $classi);
+
+header("Content-type: application/json");
+$res = json_encode($response);
+echo $res;
+exit;
